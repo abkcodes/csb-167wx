a2072e16810c9d560ea8e7036fb28e1e9525d3bf
Code Sandbox <hello@codesandbox.io>
2021-07-21 21:02:58 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6f249842e3ea36f69e2ca704c5556e4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068edf3d5e86f1f186944e97b996bc31",
+    "url": "https://abkcodes.github.io/csb-167wx/index.html"
+  },
+  {
+    "revision": "6d1cec2338c41eb7ccea",
+    "url": "https://abkcodes.github.io/csb-167wx/static/js/2.7996e7bb.chunk.js"
+  },
+  {
+    "revision": "af33f596fd0a8b958773",
+    "url": "https://abkcodes.github.io/csb-167wx/static/js/main.e0c9d223.chunk.js"
+  },
+  {
+    "revision": "b8f7c209c256509a888c",
+    "url": "https://abkcodes.github.io/csb-167wx/static/js/runtime-main.08a2fe8f.js"
+  }
+]);
